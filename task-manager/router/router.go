a6e4f66bd3ffe68f26fa73daef833039b4da8f30
Abel Zereabruk <abelazereabruk@gmail.com>
2025-03-29 20:02:50 +0300

    protect routes that needs admin access

--- task-manager/router/router.go
+++ task-manager/router/router.go
@@ -1,18 +1,24 @@
 package router
 
-import(
-	"github.com/gin-gonic/gin"
+import (
 	"task-manager/controllers"
+	"task-manager/middleware"
+
+	"github.com/gin-gonic/gin"
 )
 
 func InitializeRouter(){
 	router := gin.Default()
 
-	router.GET("/tasks",controllers.GetTasks)
-	router.GET("/tasks/:id",controllers.GetSpecificTask)
-	router.PUT("/tasks/:id",controllers.UpdateTask)
-	router.POST("/tasks",controllers.CreateTask)
-	router.DELETE("/tasks/:id",controllers.DeleteTask)
+	router.POST("/register",controllers.Register)
+	router.POST("/login",controllers.Login)
+
+	
+	router.GET("/tasks",middleware.AuthMiddleware(),controllers.GetTasks)
+	router.GET("/tasks/:id",middleware.AuthMiddleware(),controllers.GetSpecificTask)
+	router.PUT("/tasks/:id",middleware.AuthMiddleware(),middleware.AdminOnly(),controllers.UpdateTask)
+	router.POST("/tasks",middleware.AuthMiddleware(),controllers.CreateTask)
+	router.DELETE("/tasks/:id",middleware.AuthMiddleware(),middleware.AdminOnly(),controllers.DeleteTask)
 
 	router.Run("localhost:8080")
 }
