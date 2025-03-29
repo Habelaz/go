4657a8471d1fe71424f7d3db1b2a744394365ed7
Abel Zereabruk <abelazereabruk@gmail.com>
2025-03-29 20:01:31 +0300

    add user id to task.go

--- task-manager/models/task.go
+++ task-manager/models/task.go
@@ -6,5 +6,6 @@ type Task struct {
 	ID          primitive.ObjectID `bson:"_id,omitempty"`
 	Title       string             `bson:"title" json:"title"`
 	Description string             `bson:"description" json:"description"`
+	UserID 		primitive.ObjectID `bson:"user_id" json:"user_id"`
 }
 
